ef906d08dfe75c954a7cceabbfb7bc2cac82edce
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-03-15 16:41:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,45 +17,45 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  # board.each_with_index { |space, index|
-  #   space = board[index]
-  #   if space == board[index+1] && space == board[index+2]
-  #     return true
-  #   elsif space == board[index-1] && space == board[index-2]
-  #     return true
-  #   elsif space == board[index-2] && space == board[index-4]
-  #     return true
-  #   elsif board.all?{ |space| space == "X" || space == "O"}
-  #     return false
-  #   elsif board.all?{ |space| space==" "}
-  #     return false
-  #   end
-  # }
+  board.each_with_index { |space, index|
+    space = board[index]
+    if space == board[index+1] && space == board[index+2]
+      return true
+    elsif space == board[index-1] && space == board[index-2]
+      return true
+    elsif space == board[index-2] && space == board[index-4]
+      return true
+    elsif board.all?{ |space| space == "X" || space == "O"}
+      return false
+    elsif board.all?{ |space| space==" "}
+      return false
+    end
+  }
 end
 
-def won?(board)
-  if board.all?{ |space| space == " " }
-    return false
-  elsif board[0] == "X" && board[1] == "X" && board[2] == "X"
-    return WIN_COMBINATIONS[0]
-  elsif (board[3] == "X" && board[4] == "X" && board[5] == "X") || (board[3] == "O" && board[4] == "O" && board[5] == "O")
-    return WIN_COMBINATIONS[1]
-  elsif board[6] == "X" && board[7] == "X" && board[8] == "X"
-    return WIN_COMBINATIONS[2]
-  elsif board[0] == "O" && board[3] == "O" && board[6] == "O"
-    return WIN_COMBINATIONS[3]
-  elsif board[1] == "O" && board[4] == "O" && board[7] == "O"
-    return WIN_COMBINATIONS[4]
-  elsif board[2] == "O" && board[5] == "O" && board[8] == "O"
-    return WIN_COMBINATIONS[5]
-  elsif board[0] == "X" && board[4] == "X" && board[8] == "X"
-    return WIN_COMBINATIONS[6]
-  elsif board[2] == "O" && board[4] == "O" && board[6] == "O"
-    return WIN_COMBINATIONS[7]
-  elsif board.all?{ |space| space == "X" || space == "O" }
-    return false
-  end
-end
+# def won?(board)
+#   if board.all?{ |space| space == " " }
+#     return false
+#   elsif board[0] == "X" && board[1] == "X" && board[2] == "X"
+#     return WIN_COMBINATIONS[0]
+#   elsif (board[3] == "X" && board[4] == "X" && board[5] == "X") || (board[3] == "O" && board[4] == "O" && board[5] == "O")
+#     return WIN_COMBINATIONS[1]
+#   elsif board[6] == "X" && board[7] == "X" && board[8] == "X"
+#     return WIN_COMBINATIONS[2]
+#   elsif board[0] == "O" && board[3] == "O" && board[6] == "O"
+#     return WIN_COMBINATIONS[3]
+#   elsif board[1] == "O" && board[4] == "O" && board[7] == "O"
+#     return WIN_COMBINATIONS[4]
+#   elsif board[2] == "O" && board[5] == "O" && board[8] == "O"
+#     return WIN_COMBINATIONS[5]
+#   elsif board[0] == "X" && board[4] == "X" && board[8] == "X"
+#     return WIN_COMBINATIONS[6]
+#   elsif board[2] == "O" && board[4] == "O" && board[6] == "O"
+#     return WIN_COMBINATIONS[7]
+#   elsif board.all?{ |space| space == "X" || space == "O" }
+#     return false
+#   end
+# end
 
 def full?(board)
   if board.any? { |space| space == " " }
